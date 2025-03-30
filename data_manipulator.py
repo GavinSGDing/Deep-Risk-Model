3fb6aba570f4298134c556acd3f32146d3725634
Shenggang Ding <shenggan@usc.edu>
2025-03-30 02:23:16 -0700

    S&P 500 raw data

---
+++ data_manipulator.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df = pd.read_csv('stock_details_5_years.csv')
+df['Date'] = pd.to_datetime(df['Date'], utc=True)
+df = df.sort_values(by=['Company', 'Date'])
+
+new_order = ['Company', 'Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits']
+df = df[new_order]
+df['Return'] = df.groupby('Company')['Close'].pct_change()
+df.to_csv('sp500.csv', index=False)
